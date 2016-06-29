e47af44295259b50ff98a7d9ea5f5bca492f6c8e
rene <renegmed@yahoo.com>
2016-06-28 20:54:09 -0400

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+Learn creating scalable go application
---
+++ scalego/src/scalablego/web/controller/CommentController.go
@@ -0,0 +1,42 @@
+package controller
+
+import (
+	"net/http"
+	"scalego/src/scalablego/entity"
+	"scalego/src/scalablego/web/model"
+	"strconv"
+	"time"
+)
+
+type CommentController struct{}
+
+func (c *CommentController) createComment(w http.ResponseWriter, r *http.Request) {
+	matches := commentsPath.FindStringSubmatch(r.URL.Path)
+
+	//no need to check for error since regex guarantees an integer value
+	postID, _ := strconv.Atoi(matches[1])
+
+	r.ParseForm()
+	now := time.Now()
+	comment := &entity.Comment{
+		ContentItem: entity.ContentItem{
+			Subject:     r.FormValue("subject"),
+			Body:        r.FormValue("body"),
+			Author:      nil,
+			Comments:    []entity.Comment{},
+			CreatedDate: &now,
+			PublishDate: &now,
+			IsPublished: true,
+		},
+	}
+
+	comment, err := model.CreateComment(comment, postID)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("Location", "/posts/"+strconv.Itoa(postID))
+	w.WriteHeader(http.StatusSeeOther)
+}
